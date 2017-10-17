5c0fa561b857333945f0fac481ea95716d035232
mattg317 <mattg317@gmail.com>
2017-10-17 16:47:46 -0400

    added html

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ app = Flask("__main__")
 
 @app.route("/")
 def home():
-		print("Retrieving homepage")
-		return "Welcome to my home page"
+    print("Retrieving homepage")
+    return "Welcome to my home page"
+
+
+@app.route("/about")
+def about():
+    print("Retrieving about page")
+    return app.send_static_file('index.html')
+
 
 if __name__ == ("__main__"):
-		app.run()
+    app.run()
